534788f5ef870ce63e9dec1b61d32185cda1ccbf
jiyoonyee <a01037651377@gmail.com>
2025-07-25 11:41:47 +0900

    fix: config 수정

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    allowedHosts: ['dbase.o-r.kr'],
+    host: true,
+  },
+}))
